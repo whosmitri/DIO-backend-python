849bb3f0032d09bde0ab2cd059a76924d4cf71a0
whosmitri <glizqc06@gmail.com>
2025-11-09 10:54:44 -0300

    renamed my super license plate, pow pow

--- module_02/01-functions/03-arguments.py
+++ module_02/01-functions/03-arguments.py
@@ -5,15 +5,15 @@ def save_car(model, brand, year, license_plate):
 
 ###
 
-# Ferrari / LaFerrari / 2013 / XYZ-0001
+# Ferrari / LaFerrari / 2013 / DMT-2402
 print('1:')
-save_car("Ferrari", "LaFerrari", 2013, "XYZ-0001")
-# LaFerrari / Ferrari / 2013 / XYZ-0001
+save_car("Ferrari", "LaFerrari", 2013, "DMT-2402")
+# LaFerrari / Ferrari / 2013 / DMT-2402
 
 print('\n2:')
-save_car(brand="Ferrari", model="LaFerrari", year=2013, license_plate="XYZ-0001")
-# Ferrari / LaFerrari / 2013 / XYZ-0001
+save_car(brand="Ferrari", model="LaFerrari", year=2013, license_plate="DMT-2402")
+# Ferrari / LaFerrari / 2013 / DMT-2402
 
 print('\n3:')
-save_car(**{"brand": "Ferrari", "model": "LaFerrari", "license_plate": "XYZ-0001", "year": 2013})
-# Ferrari / LaFerrari / 2013 / XYZ-0001
+save_car(**{"brand": "Ferrari", "model": "LaFerrari", "license_plate": "DMT-2402", "year": 2013})
+# Ferrari / LaFerrari / 2013 / DMT-2402
